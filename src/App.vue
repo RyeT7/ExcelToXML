d830d871b3e6a309eb44d6e8b7f2cb929fb4ccdd
Rye <154753794+RyeT7@users.noreply.github.com>
2026-04-06 00:37:04 +0700

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+
+const excelView = ref<string>(JSON.parse("{}"));
+const excel = ref<File>();
+
+async function convert() {
+  await invoke("convert", {});
+}
+
+async function viewFile() {
+  excelView.value = await invoke("view_file", {});
+}
+</script>
+
+<template>
+  <main class="">
+    <h1>Excel to XML Converter</h1>
+    <form action="" method="post">
+      <input type="file" name="" id="file-uploader" accept=".xlsx;.csv"/>
+      <div>
+        <button type="submit" >Convert</button>
+        <button type="button">View File</button>
+      </div>
+    </form>
+  </main>
+</template>
