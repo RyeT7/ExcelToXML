a3e54570ebbc0431aa3ebb7901b09ee4d3b57285
RyeT7 <stanley05ryuu@gmail.com>
2026-04-08 15:15:41 +0700

    fix: excel reader

--- src-tauri/src/excel/reader.rs
+++ src-tauri/src/excel/reader.rs
@@ -1,18 +1,16 @@
 use std::{collections::HashMap, fs::File, io::BufReader};
 
-use calamine::{Reader, Sheets, open_workbook_auto};
-
-use crate::excel::readerResponse::{ExcelReaderResponse, ExcelReaderResponseTrait};
+use calamine::{Data, Reader, Sheets, open_workbook_auto};
 
 pub struct ExcelReader<'a> {
     path: &'a str,
     workbook: Sheets<BufReader<File>>,
-    data_row: Vec<HashMap<&'a str, &'a str>>,
+    pub data_row: Vec<HashMap<String, String>>,
 }
 
 pub trait ExcelReaderTrait<'a> {
     fn new(path: &'a str) -> Result<ExcelReader<'a>, &'static str>;
-    fn read_excel(&mut self, sheet_names: Vec<&str>) -> ExcelReaderResponse;
+    fn read_excel(&mut self) -> Result<(), &'static str>;
 }
 
 impl<'a> ExcelReaderTrait<'a> for ExcelReader<'a> {
@@ -31,19 +29,37 @@ impl<'a> ExcelReaderTrait<'a> for ExcelReader<'a> {
 
     }
     
-    fn read_excel(&mut self, sheet_names: Vec<&str>) -> ExcelReaderResponse {
-        let mut response = ExcelReaderResponse::new();
-        
-        for sheet in sheet_names {
-            let range = self.workbook.worksheet_range(sheet);
-
-            if let Err(_) = range {
-                response.add_error(
-                    format!("{} does not exist within the workbook", sheet)
-                );
+    fn read_excel(&mut self) -> Result<(), &'static str> {
+        let range = match self.workbook.worksheet_range_at(0) {
+            Some(Ok(range)) => range,
+            Some(Err(_)) => {
+                return Err("Something went wrong: sheet not found in workbook")
+            },
+            None => {
+                return Err("There are no sheets on this workbook")
             }
+        };
+
+        for row in range.rows() {
+            let mut row_map: HashMap<String, String> = HashMap::new();
+
+            for (col_idx, cell) in row.iter().enumerate() {
+                let col_name = format!("Column{}", col_idx);
+
+                let cell_value = match cell {
+                    Data::String(s) => s.to_string(),
+                    Data::DateTime(d) => d.to_string(),
+                    Data::Float(f) => f.to_string(),
+                    Data::Int(i) => i.to_string(),
+                    _ => String::from("")
+                };
+
+                row_map.insert(col_name, cell_value);
+            }
+
+            self.data_row.push(row_map);
         }
 
-        response
+        Ok(())
     }
 }
